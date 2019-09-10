8123623cf815bf0c73244dc8aed97d4ecbcb2af6
Pavel Kozlov <pavelcauselov@gmail.com>
2019-09-10 12:49:59 +0300

    next

--- Chapter 5/Challenge.playground/Contents.swift
+++ Chapter 5/Challenge.playground/Contents.swift
@@ -3,43 +3,45 @@
 import RxSwift
 
 example(of: "Challenge 1") {
-
+    
     let disposeBag = DisposeBag()
-
+    
     let contacts = [
-    "603-555-1212": "Florent",
-    "212-555-1212": "Junior",
-    "408-555-1212": "Marin",
-    "617-555-1212": "Scott"
+        "603-555-1212": "Florent",
+        "212-555-1212": "Junior",
+        "408-555-1212": "Marin",
+        "617-555-1212": "Scott"
     ]
-
+    
     func phoneNumber(from inputs: [Int]) -> String {
-    var phone = inputs.map(String.init).joined()
-
-    phone.insert("-", at: phone.index(
-        phone.startIndex,
-        offsetBy: 3)
-    )
-
-    phone.insert("-", at: phone.index(
-        phone.startIndex,
-        offsetBy: 7)
-    )
+        var phone = inputs.map(String.init).joined()
+        
+        phone.insert("-", at: phone.index(
+            phone.startIndex,
+            offsetBy: 3)
+        )
+        
+        phone.insert("-", at: phone.index(
+            phone.startIndex,
+            offsetBy: 7)
+        )
         return phone
     }
-
+    
     let input = PublishSubject<Int>()
-
+    
     // Add your code here
-
+    
     input
         .skipWhile({ $0 == 0 })
         .filter({ $0 < 10 })
         .take(10)
         .toArray()
+        .debug("B")
         .subscribe(onNext: {
             print("onNext is - \($0)")
             let phone = phoneNumber(from: $0)
+            print("phone", phone)
             if let contact = contacts[phone] {
                 print("Dialing \(contact) (\(phone))...")
             } else {
@@ -50,13 +52,13 @@ example(of: "Challenge 1") {
     
     input.onNext(0)
     input.onNext(603)
-
+    
     input.onNext(2)
     input.onNext(1)
-
+    
     // Confirm that 7 results in "Contact not found", and then change to 2 and confirm that Junior is found
     input.onNext(2)
-
+    
     "5551212".forEach {
         if let number = (Int("\($0)")) {
             input.onNext(number)
